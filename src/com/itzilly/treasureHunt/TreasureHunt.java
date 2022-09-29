d2e9d57a89dd93a91f0cd7057e0ad72168f2705f
itzilly <itzillyum@gmail.com>
2022-09-28 19:31:09 -0600

    Update version/build numbers
    alpha-0.0.1-b2

--- src/com/itzilly/treasureHunt/TreasureHunt.java
+++ src/com/itzilly/treasureHunt/TreasureHunt.java
@@ -2,8 +2,8 @@ package com.itzilly.treasureHunt;
 
 
 public class TreasureHunt {
-    public static String VERSION = "alpha-0.0.1";
-    public static String BUILD_VERSION = "b1";
+    public static String VERSION = "alpha-0.0.2";
+    public static String BUILD_VERSION = "b2";
     public static TreasureHuntWindow treasureHuntWindow = new TreasureHuntWindow("Treasure Hunt v" + VERSION + BUILD_VERSION);
 
     public static void main(String[] args) {
