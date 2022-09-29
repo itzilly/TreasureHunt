ccd9644bae4ebc3ee8f0077bfdaa5d9dc21fc65f
itzilly <itzillyum@gmail.com>
2022-09-28 23:59:42 -0600

    Add animations

--- src/com/itzilly/treasureHunt/ui/GamePanel.java
+++ src/com/itzilly/treasureHunt/ui/GamePanel.java
@@ -5,13 +5,34 @@ import java.awt.*;
 
 public class GamePanel extends JPanel {
 
+    private Timer timer;
+
     public GamePanel() {
         initializeLayout();
+        startAnimation();
     }
 
     private void initializeLayout() {
         setPreferredSize(new Dimension(600, 600));
         setFocusable(true);
+        setBackground(Color.BLACK);
     }
 
+    private void startAnimation() {
+        timer = new Timer(1000, new GameLoop(this));
+        timer.start();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        handleCanvas(g);
+    }
+
+    private void handleCanvas(Graphics g) {
+    }
+
+    public void loop() {
+        repaint();
+    }
 }
